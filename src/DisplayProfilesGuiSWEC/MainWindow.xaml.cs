84827198f0db52421f3684e766991e34616d1dc1
Jeremy Wong <navhaxs@hotmail.com>
2018-01-29 16:44:58 +1100

    Checkin custom gui

---
+++ src/DisplayProfilesGuiSWEC/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using DisplayProfiles;
+using DisplayProfiles.Interop;
+
+namespace DisplaySwitcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string profileName;
+            if (powerpointMode.IsChecked == true)
+            {
+                profileName = "POWERPOINT";
+            } else if(propresenterMode.IsChecked == true) {
+                profileName = "PROPRESENTER";
+            } else {
+                return;
+            }
+
+            {
+                DisplaySettings profile;
+                try
+                {
+
+                    profile = ProfileFiles.LoadProfile(profileName);
+
+                    var extraMessage = profile.MissingAdaptersMessage();
+                    if (extraMessage != "")
+                        Console.Error.WriteLine("Warning: " + extraMessage);
+                    profile.SetCurrent();
+                } catch (System.IO.FileNotFoundException ex)
+                {
+                    var m = new MessageBox();
+                    m.ShowMessage(this, "Profiles are missing", "To fix this error", "Create the POWERPOINT and PROPRESENTER profiles from DisplayProfilesGui.exe");
+                } catch (Exception ex)
+                {
+                    var m = new MessageBox();
+                    m.ShowMessage(this, "Error", "Something happened", ex.Message);
+                    throw;
+                }
+            }
+        }
+
+        private void any_mode_Checked(object sender, RoutedEventArgs e)
+        {
+            applyButton.IsEnabled = true;
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("ms-settings:display");
+        }
+
+        private void Hyperlink_Click_1(object sender, RoutedEventArgs e)
+        {
+            var m = new MessageBox();
+            m.ShowMessage(this, "Help", "To reconfigure profiles...", "Use DisplayProfilesGui.exe to edit the POWERPOINT and PROPRESENTER profiles.");
+        }
+    }
+}
+ 
